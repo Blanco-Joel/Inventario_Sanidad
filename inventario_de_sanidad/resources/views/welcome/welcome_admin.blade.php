cf18b6f2767318c56016872572d706ee29717c3b
xNickqo <nikolaslf007@gmail.com>
2025-05-16 17:22:02 +0200

    feat: continuando con los estilos globales

--- inventario_de_sanidad/resources/views/welcome/welcome_admin.blade.php
+++ inventario_de_sanidad/resources/views/welcome/welcome_admin.blade.php
@@ -4,12 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienvenido al Portal del departamento de Sanidad</title>
+    <link rel="stylesheet" href="{{ asset('css/normalize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/global.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style_welcome.css') }}">
 
 </head>
 <body>
+
+    <!-- Dialog para cambiar contraseña -->
     <dialog id="firstLogDialog" >
-        <div  class="text-center" >
+        <div class="dialog-content">
             <h3>Cambiar Contraseña</h3>
             <p>En el primer ingrso a la página se ha<br> de cambiar la contraseña.</p>
             <form id="FirstLogForm"  action="{{ route('changePasswordFirstLog') }}" method="POST">
@@ -26,29 +30,40 @@
             </form>
         </div>
     </dialog>
-    <div class="container">
-        <h1 class="text-center">Portal del departamento de Sanidad</h1> 
-        
-        <div class="card ">
-            <div class="header">Menú de administradores </div><br>
-                <div id="name"><b>Bienvenido/a: </b> {{ Cookie::get('NAME') }}</div><br><br>
-                <b>Identificador Empleado: </b> {{ Cookie::get('USERPASS') }}<br><br>
-                @if (session('mensaje'))
-                    <p>{{ session('mensaje') }}</p>
-                @endif
-                <!-- Botones del menú -->
-                <button onclick="window.location.href='{{ route('users.usersManagement') }}'" class="btn btn-warning">Gestión de usuarios</button>
-                <button onclick="window.location.href='{{ route('materials.dashboard') }}'" class="btn btn-warning">Gestión de materiales</button>
-                <button onclick="window.location.href='{{ route('historical.historicalSubmenu') }}'" class="btn btn-warning">Reservas de Materiales</button>
-                <br><br>
 
+
+    <!-- Contenedor principal -->
+    <div class="container">
+        <div class="card">
+            <div class="header">Menú de administradores </div>
+
+            <div id="name"><b>Bienvenido/a: </b> {{ Cookie::get('NAME') }}</div>
+            
+            <div id="id"><b>Identificador Empleado: </b> {{ Cookie::get('USERPASS') }}</div> 
+            
+            @if (session('mensaje'))
+                <p>{{ session('mensaje') }}</p>
+            @endif
+            
+            <!-- Botones del menú -->
+            <div class="btn-group">
+                <a href="{{ route('users.usersManagement') }}" class="btn btn-primary">Gestión de usuarios</a>
+                <a href="{{ route('materials.dashboard') }}" class="btn btn-primary">Gestión de materiales</a>
+                <a href="{{ route('historical.historicalSubmenu') }}" class="btn btn-primary">Reservas de Materiales</a>
+            </div>
+            
+            <div class="logout">
                 <a href="{{ route('logout') }}" class="btn btn-danger">Cerrar Sesión</a>
-                @if($data->isNotEmpty())
-                    <h4>WARNING</h4>
-                    @foreach ($data as $warning)
-                        <p>{{$warning->name}} tiene solo {{$warning->units}} unidad/es en {{$warning->storage_type ==  "use" ? "uso" : "reserva";}}.</p>
-                    @endforeach
-                @endif
+            </div>
+            
+            
+            <!-- Notificaciones -->
+            @if($data->isNotEmpty())
+                <h4>WARNING</h4>
+                @foreach ($data as $warning)
+                    <p>{{$warning->name}} tiene solo {{$warning->units}} unidad/es en {{$warning->storage_type ==  "use" ? "uso" : "reserva";}}.</p>
+                @endforeach
+            @endif
         </div>
         
     </div>
