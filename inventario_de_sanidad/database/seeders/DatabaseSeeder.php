8714bf09bdc3c17234ac1b521353c36fd9966170
joel Blanco <joelb5700@gmail.com>
2025-06-13 13:58:26 +0200

    feat: card use en alumnos

--- inventario_de_sanidad/database/seeders/DatabaseSeeder.php
+++ inventario_de_sanidad/database/seeders/DatabaseSeeder.php
@@ -204,15 +204,15 @@ class DatabaseSeeder extends Seeder
 
         // Seed Modifications
         DB::table('modifications')->insert([
-            ['user_id' => 1, 'material_id' => 1, 'storage' => 'CAE', 'storage_type' => 'reserve',  'units' => 10, 'action_datetime' => now()],
-            ['user_id' => 2, 'material_id' => 2, 'storage' => 'CAE', 'storage_type' => 'use', 'units' => -5, 'action_datetime' => now()->subDays(5)],
-            ['user_id' => 3, 'material_id' => 1, 'storage' => 'CAE', 'storage_type' => 'reserve', 'units' => -3, 'action_datetime' => now()->subDays(10)],
-            ['user_id' => 1, 'material_id' => 1, 'storage' => 'CAE', 'storage_type' => 'reserve', 'units' => 15, 'action_datetime' => now()->subDays(3)],
-            ['user_id' => 2, 'material_id' => 2, 'storage' => 'CAE', 'storage_type' => 'use',  'units' => -5, 'action_datetime' => now()->subDays(7)],
-            ['user_id' => 3, 'material_id' => 1, 'storage' => 'CAE', 'storage_type' => 'reserve', 'units' => -8, 'action_datetime' => now()->subDays(2)],
-            ['user_id' => 1, 'material_id' => 1, 'storage' => 'odontology', 'storage_type' => 'reserve',  'units' => 12, 'action_datetime' => now()->subDays(4)],
-            ['user_id' => 3, 'material_id' => 3, 'storage' => 'odontology', 'storage_type' => 'use', 'units' => -4, 'action_datetime' => now()->subDays(6)],
-            ['user_id' => 1, 'material_id' => 7, 'storage' => 'odontology', 'storage_type' => 'use', 'units' => 9, 'action_datetime' => now()->subDays(1)],
+            ['user_id' => 8, 'material_id' => 1, 'storage' => 'CAE', 'storage_type' => 'reserve',  'units' => 10, 'action_datetime' => now()],
+            ['user_id' => 6, 'material_id' => 2, 'storage' => 'CAE', 'storage_type' => 'use', 'units' => -5, 'action_datetime' => now()->subDays(5)],
+            ['user_id' => 9, 'material_id' => 1, 'storage' => 'CAE', 'storage_type' => 'reserve', 'units' => -3, 'action_datetime' => now()->subDays(10)],
+            ['user_id' => 8, 'material_id' => 1, 'storage' => 'CAE', 'storage_type' => 'reserve', 'units' => 15, 'action_datetime' => now()->subDays(3)],
+            ['user_id' => 7, 'material_id' => 2, 'storage' => 'CAE', 'storage_type' => 'use',  'units' => -5, 'action_datetime' => now()->subDays(7)],
+            ['user_id' => 9, 'material_id' => 1, 'storage' => 'CAE', 'storage_type' => 'reserve', 'units' => -8, 'action_datetime' => now()->subDays(2)],
+            ['user_id' => 9, 'material_id' => 1, 'storage' => 'odontology', 'storage_type' => 'reserve',  'units' => 12, 'action_datetime' => now()->subDays(4)],
+            ['user_id' => 6, 'material_id' => 3, 'storage' => 'odontology', 'storage_type' => 'use', 'units' => -4, 'action_datetime' => now()->subDays(6)],
+            ['user_id' => 9, 'material_id' => 7, 'storage' => 'odontology', 'storage_type' => 'use', 'units' => 9, 'action_datetime' => now()->subDays(1)],
         ]);
 
         // Seed Activities
